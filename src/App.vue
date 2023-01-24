ac08f37695142e627c30daaf9d39ef6c0a9c491e
Lydia Ego <egolydia97@gmail.com>
2023-01-24 16:54:17 +0100

    Feat: import dependencies

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <router-view />
+</template>
+
+<script setup>
+import { useUserStore } from './store/user-store'
+import { useRouter } from 'vue-router'
+import { onMounted } from 'vue';
+const userStore = useUserStore();
+const router = useRouter();
+
+</script>
