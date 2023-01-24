edd2961299eca48f8716f70f8ddd447afa5ee2bd
Lydia Ego <egolydia97@gmail.com>
2023-01-24 17:03:51 +0100

    Feat: implement saving user details if isFirst time login

--- src/store/user-store.js
+++ src/store/user-store.js
@@ -20,6 +20,7 @@ export const useUserStore = defineStore('user', {
                     token: data.credential
                 })
 
+                if (!userExists) await this.saveUserDetails(res);
 
                 this.sub = res.data.sub
                 this.email = res.data.email
@@ -29,6 +30,20 @@ export const useUserStore = defineStore('user', {
             } catch (e) {
                 console.log(e);
             }
+        },
+        async saveUserDetails(res) {
+            try {
+                await setDoc(doc(db, 'users', res.data.sub), {
+                    sub: res.data.sub,
+                    email: res.data.email,
+                    picture: res.data.picture,
+                    firstName: res.data.given_name,
+                    lastName: res.data.family_name
+                })
+            } catch (e) {
+                console.log(e);
+            }
+
         },
         logout() {
             this.sub = '',
