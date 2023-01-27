e24665b8388fc4f5048f086aaeb0c01bbc552af2
Lydia Ego <egolydia97@gmail.com>
2023-01-27 10:24:53 +0100

    Feat: implement logic when a chat is opened

--- src/views/ChatsView.vue
+++ src/views/ChatsView.vue
@@ -6,6 +6,36 @@
 
 <script setup>
 import MessageRowComponent from '../components/MessageRowComponent.vue';
+const openChat = async (chat) => {
+    userDataForChat.value = []
+    userDataForChat.value.push({
+        id: chat.id,
+        sub1: chat.sub1,
+        sub2: chat.sub2,
+        firstName: chat.user.firstName,
+        lastName: chat.user.lastName,
+        picture: chat.user.picture,
+    })
+    try {
+        await userStore.getChatById(chat.id)
+        let data = {
+            id: chat.id,
+            key1: 'sub1HasViewed', val1: false,
+            key2: 'sub2HasViewed', val2: false,
+
+        }
+        if (chat.sub1 == sub.value) {
+            data.val1 = true
+            data.val2 = true
+        }else if (chat.sub2 == sub.value) {
+            data.val1 = true
+            data.val2 = true
+        }
+        await userStore.hasReadMessage(data)
+    } catch (e) {
+        console.log(e);
+    }
+}
 </script>
 
 <style lang="scss" scoped>
